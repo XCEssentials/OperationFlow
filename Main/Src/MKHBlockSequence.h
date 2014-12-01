6d515753098fd8e9a5be6d53f1f3ae225a963d6c
maximkhatskevich <maxim.khatskevich@gmail.com>
2014-12-02 00:32:45 +0500

    Initial files have been added.

---
+++ Main/Src/MKHBlockSequence.h
@@ -0,0 +1,13 @@
+//
+//  MKHBlockSequence.h
+//  BlockSequence
+//
+//  Created by Maxim Khatskevich on 02/12/14.
+//  Copyright (c) 2014 Maxim Khatskevich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKHBlockSequence : NSObject
+
+@end
---
+++ Main/Src/MKHBlockSequence.m
@@ -0,0 +1,13 @@
+//
+//  MKHBlockSequence.m
+//  BlockSequence
+//
+//  Created by Maxim Khatskevich on 02/12/14.
+//  Copyright (c) 2014 Maxim Khatskevich. All rights reserved.
+//
+
+#import "MKHBlockSequence.h"
+
+@implementation MKHBlockSequence
+
+@end
