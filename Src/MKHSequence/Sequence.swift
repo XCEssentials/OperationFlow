4a74ca6413176bfb99ee8ae5091032beb023edf1
maximkhatskevich <maxim@khatskevi.ch>
2015-11-26 17:35:47 -0800

    Setup project structure

---
+++ Src/MKHSequence/Sequence.swift
@@ -0,0 +1,14 @@
+//
+//  Sequence.swift
+//  MKHSequence
+//
+//  Created by Maxim Khatskevich on 11/26/15.
+//  Copyright © 2015 Maxim Khatskevich. All rights reserved.
+//
+
+import UIKit
+
+class Sequence
+{
+    //
+}
