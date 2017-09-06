85f3e518e3e29429750f0defa097b645413f2597
Maxim Khatskevich <maxim@khatskevi.ch>
2017-09-07 04:48:37 +0500

    Added ‘Promise’ class

---
+++ Sources/Promise.swift
@@ -0,0 +1,38 @@
+protocol DeferredResult
+{
+    var value: Any? { get }
+    
+    var onSuccess: ((Any?) -> Void)? { get set }
+    var onFailure: ((Error) -> Void)? { get set }
+}
+
+//===
+
+public
+final
+class Promise<T>: DeferredResult
+{
+    var value: Any?
+    
+    //===
+    
+    var onSuccess: ((Any?) -> Void)?
+    var onFailure: ((Error) -> Void)?
+}
+
+//===
+
+public
+extension Promise
+{
+    func success(with result: T)
+    {
+        value = result
+        onSuccess?(result)
+    }
+    
+    func failure(with error: Error)
+    {
+        onFailure?(error)
+    }
+}
